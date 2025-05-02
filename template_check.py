8669f2f26a8010e8ca76490c44409caa52be3a3e
Sihan0229 <sihangao2004@gmail.com>
2025-05-03 00:58:21 +0800

    vertice检查注解

--- template_check.py
+++ template_check.py
@@ -3,3 +3,9 @@ import nibabel as nib
 surf = nib.load("/root/autodl-tmp/hcp1200_dataset/HCP1200_cut/100408/100408.R.white.native.surf.gii")
 vertices = surf.darrays[0].data
 print("Template vertices:", vertices.shape[0])
+
+# 模板当中
+# 不加150k的surf都是163842，和sphere一样的尺寸
+# 加了150k的大小不一，已经删掉了
+
+# HCP当中，各个都不太一样大，需要remesh
